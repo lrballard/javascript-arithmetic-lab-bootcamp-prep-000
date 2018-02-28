f3b56b6f078ff25a7834d80d103fd44a379caffe
Lynn Ballard <lynn@pingsplash.com>
2018-02-28 20:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 60 - 40 // 20
 2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
 5.0 / 2.5 // 2
-function add()
+//function add()
+//function subract()
+//function multiply()
+//function divide()
+var x = 1;
+var y = 80;
+var z = x + y;
+document.function add() {
+  return z
+}
